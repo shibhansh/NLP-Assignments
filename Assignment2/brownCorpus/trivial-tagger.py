19767aced8f76228f488c6f322983660d0d72663
shibhansh <shibhanshdohare1997@gmail.com>
2016-10-02 18:54:34 +0530

    fix/default-tagger

--- Assignment2/brownCorpus/trivial-tagger.py
+++ Assignment2/brownCorpus/trivial-tagger.py
@@ -3,16 +3,17 @@ import nltk
 from nltk.corpus import brown
 
 #getting the tagged sentences
-brown_tagged_sents = brown.tagged_sents()
-brown_sents = brown.sents()
+brown_tagged_words = brown.tagged_words()
 
 #distributing the data in test and train set
-size = int(len(brown_tagged_sents) * 0.9)
-train_sents = brown_tagged_sents[:size]
-test_sents = brown_tagged_sents[size:]
+no_of_words = int(len(brown_tagged_words) * 0.9)
+train_words = brown_tagged_words[:no_of_words]
+
+#due to the format of evaluation of nltk
+test_words = [brown_tagged_words[no_of_words:]]
 
 #getting the tag distribution
-tags_distribution = [tag for (word, tag) in brown.tagged_words()]
-tagger = nltk.DefaultTagger(nltk.FreqDist(tags_distribution).max())
+tag_distribution = [tag for (word, tag) in train_words]
+tagger = nltk.DefaultTagger(nltk.FreqDist(tag_distribution).max())
 
-print tagger.evaluate(test_sents)
+print tagger.evaluate(test_words)
