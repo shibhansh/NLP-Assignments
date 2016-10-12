84050815f0d92e99444abf1775b7b99b10487eb4
shibhansh <shibhanshdohare1997@gmail.com>
2016-10-12 22:19:09 +0530

    add/ass2/svm.py

--- Assignment2/imdb_movie_review/aclImdb/binary-BoW.py
+++ Assignment2/imdb_movie_review/aclImdb/binary-BoW.py
@@ -1,6 +1,6 @@
 from __future__ import division
 import nltk
-import gensim
+# import gensim
 import sys
 import numpy as np
 import pickle
@@ -15,10 +15,10 @@ if len(sys.argv) > 3:
 file_name = sys.argv[1]
 print file_name
 
-file_name = open("test_neg.txt", "rb")
+file = open(file_name, "rb")
 
 #store the docs in a list
-document = file_name.readlines()
+document = file.readlines()
 #removing the extra newline character in every dcoument and word
 document = [x[:-1] for x in document]
 
@@ -26,23 +26,18 @@ vocabulary = open("imdb.vocab", "rb")
 vocab = vocabulary.readlines()
 vocab = [x[:-1] for x in vocab]
 
-dictionary = {}
-count = 0
-for word in vocab:
-	dictionary[word] = count
-	count = count + 1
-
 count = 0
 
 bBoW_file = open("bBoW/"+file_name+".bBoW", "wb")
 
+number_of_documents = len(document)
 #creating vector representations for documents
 for doc in document:
 	document[count] = nltk.word_tokenize(doc)
 	doc = document[count]
-	print count
+	print("Documents Processed %d, remaining %d" % (count, number_of_documents-count))
 	count = count + 1
-	current_BoW = [0]*len(vocab)	
+	current_BoW = [0]*30000
 	for word in doc:
 		key_location = 0
 		for key in vocab:
@@ -50,6 +45,8 @@ for doc in document:
 				current_BoW[key_location] = 1
 				break
 			key_location = key_location + 1
+			if key_location >= 30000:
+				break
 	pickle.dump(current_BoW, bBoW_file)
 
 	# file.write("%s\n" % current_BoW)
---
+++ Assignment2/imdb_movie_review/aclImdb/svm.py
@@ -0,0 +1,65 @@
+from __future__ import division
+import numpy as np
+import pickle
+from sklearn import svm
+
+with open("w2v/train_neg.txt.w2v", 'rb') as file:
+	list_train_neg = pickle.load(file)
+print "_________loaded negative train file___________"
+
+with open("w2v/train_pos.txt.w2v", 'rb') as file:
+	list_train_pos = pickle.load(file)
+print "_________loaded positive train file___________"
+
+with open("w2v/test_neg.txt.w2v", 'rb') as file:
+	list_test_neg = pickle.load(file)
+print "__________loaded negative test file___________"
+
+with open("w2v/test_pos.txt.w2v", 'rb') as file:
+	list_test_pos = pickle.load(file)
+print "__________loaded positive test file___________"
+
+
+print "___________creating training data______________"
+data_train = np.ndarray(shape=(len(list_train_pos)+len(list_train_neg),300))
+data_train_target = np.ndarray(shape=len(list_train_pos)+len(list_train_neg))
+for i in range(0,len(list_train_pos)):
+	data_train[i]=list_train_pos[i]
+	data_train_target[i] = 1
+
+for i in range(len(list_train_pos),len(list_train_pos)+len(list_train_neg)):
+	data_train[i]=list_train_neg[i-len(list_train_pos)]
+	data_train_target[i] = -1
+print "____________created training data______________"
+
+print "____________fitting in classifier______________"
+clf = svm.SVC(gamma=0.001, C=100.)
+clf.fit(data_train[:], data_train_target[:])  
+print "____________fitted in classifier______________"
+
+print "___________creating test data______________"
+data_test_pos = np.ndarray(shape=(len(list_test_pos),300))
+for i in range(0,len(list_test_pos)):
+	data_test_pos[i] = list_test_pos[i]
+
+data_test_neg = np.ndarray(shape=(len(list_test_neg),300))
+for i in range(0,len(list_test_neg)):
+	data_test_neg[i] = list_test_neg[i]
+print "____________created test data______________"
+
+
+print "__________________testing__________________"
+result_test_pos = clf.predict(data_test_pos[:])
+result_test_neg = clf.predict(data_test_neg[:])
+
+print "______________calculating_accuracy______________"
+count = 0
+for i in range(0,len(result_test_neg)):
+	if result_test_neg[i] == -1:
+		count = count+1
+for i in range(0,len(result_test_pos)):
+	if result_test_pos[i] 	== 1:
+		count = count+1
+
+print "accuracy: %f"  % ( count/(len(list_test_pos)+len(list_test_neg)))
+# accuracy: 0.587063
