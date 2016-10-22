8a1c416c182c1f6388d447f8bb8adafe072b2cef
shibhansh <shibhanshdohare1997@gmail.com>
2016-10-22 22:08:26 +0530

    add/basic_cky

--- Assignment3/cky.py
+++ Assignment3/cky.py
@@ -34,19 +34,36 @@ grammar['X2'] = [['Verb','NP']]
 grammar['PP'] = [['Preposition', 'NP']]
 
 
-sent = ['I','prefer','book']
+sent = ['I','book']
 count = 0
+
+mat = [[[]for x in range(len(sent))]for y in range(len(sent))]
+back = [[[]for x in range(len(sent))]for y in range(len(sent))]
+
 for word in sent:
-	for A in non_terminals:
+	for A in grammar:
 		# if A goes to word in the grammar, consider it as one of the possibilities of parse
-		if A in grammar:
-			for sub_list in grammar[A]:
-				if word in sub_list:
-					count +=1
+		for sub_list in grammar[A]:
+			if word in sub_list:
+				# add this rule in the cell
+				mat[sent.index(word)][sent.index(word)].append(A)
 
 # store how are we getting at that rule, basically store the address of it's constituents in the cell
-for span in range(2,len(sent)):
+for span in range(1,len(sent)):
 	for begin in range(0, len(sent)-span):
+		# begin & end store the address of the current cell
 		end = begin + span
-		for split in (begin+1,end-1):
+		print begin,end
+		for split in range(begin,end):
+			# These should be the indices of the places to search in
+			print begin,split,"        ",split+1,end
+			# Now search for the rule whose one part is in mat[begin][split] , mat[split+1][end] & append the result in mat[begin][end]
+			for non_terminal1 in mat[begin][split]:
+				for non_terminal2 in mat[split+1][end]:
+					for A in grammar:
+						for sub_list in grammar[A]:
+							if sub_list[0] == non_terminal1 and sub_list[1] == non_terminal2:
+								mat[begin][end].append(A)
+								back[begin][end].append([[begin,split],[split+1,end]])
+								print A, non_terminal1, non_terminal2
 			# Now fill these cells with whatever we want or need to
