e4538829051842310d694038bd5105f9d14db697
shibhansh <shibhanshdohare1997@gmail.com>
2016-10-26 02:45:00 +0530

    finish/earley's parser without backtracing

--- Assignment3/earley.py
+++ Assignment3/earley.py
@@ -1,103 +1,108 @@
-terminals = ['book', 'TWA', 'Houston', 'flight', 'meal', 'money', 'include', 'prefer','this','that','a','does','from','to','on']
-non_terminals = ['S', 'NP', 'VP', 'Aux', 'Verb', 'PP', 'Det', 'Nominal', 'Prep', 'Noun','Proper-Noun']
-grammar = {}
-grammar['S'] =  [['NP', 'VP'], ['Aux','NP','VP'],['VP']]
-grammar['NP'] = [['Det','Nominal'],['Proper-Noun']]
-grammar['Nominal'] = [['Noun'],['Noun','Nominal'],['Nominal','Prep']]
-grammar['VP'] = [['Verb']['Verb','NP']]
-grammar['Verb'] = [['book'],['include'],['prefer']]
-grammar['Det']  = [['this'],['that'],['a']]
-grammar['Noun'] = [['book'],['flight'],['meal'],['money']]
-grammar['Aux']  = [['does']]
-grammar['Prep'] = [['from'],['to'],['on']]
-grammar['Proper-Noun'] = [['Houston'],['TWA']]
-POS = {}
-POS['book']		=	['Noun','Verb']
-POS['TWA']		=	['Proper-Noun']
-POS['Houston']	=	['Proper-Noun']
-POS['flight']	=	['Noun']
-POS['meal']		=	['Noun']
-POS['money']	=	['Noun']
-POS['include']	=	['Verb']
-POS['prefer']	=	['Verb']
-POS['that']		=	['Det']
-POS['this']		=	['Det']
-POS['a']		=	['Det']
-POS['does']		= 	['Aux']
-POS['from']		=	['Prep']
-POS['to']		=	['Prep']
-POS['on']		=	['Prep']
-sent = ['book','this','flight']
-chart = [[] for i in range(0,len(sent))]
-def EarleyParser(grammar):
-	enqueue(['gamma','.','S',[0,0]],0)
-	for i in range(0,len(sent)):
-		for state in chart[i]:
-			if incomplete(state):
-				if state[state.index('.')+1] in terminals:
-					scanner(state,i+1)
-				else:
-					predictor(state)
-			else:
-				completer(state)
+import copy
 
-def enqueue(state,chart_entry):
-	print "________________enqueue________________________-----------------------------------------------------------------__"
-	Found = False
-	for sub_list in chart[chart_entry]:
-		if sub_list == state:
-			Found = True
-	if Found == False:
-		chart[chart_entry].append(state)
-		for sub_list in state:
-			print sub_list
-	print "____________________end of enqueue______________-----------------------------------------------------------------__"
+terminals = ['Papa', 'ate', 'the', 'a', 'caviar', 'spoon', 'with']
+non_terminals = ['root','S', 'NP', 'VP','Verb', 'PP','Noun','Det','P']
+grammar = {}
+grammar['root'] = [['S']]
+grammar['S']	 =  [['NP', 'VP']]
+grammar['NP']	= [['Det','Noun'],['NP','PP'],['Papa']]
+grammar['VP']	 = [['VP','PP'],['Verb','NP']]
+grammar['PP']	 = [['P','NP']]
+grammar['Verb']	 = [['ate']]
+grammar['Det']	= [['the'],['a']]
+grammar['Noun'] = [['caviar'],['spoon']]
+grammar['P']	= [['with']]
+
+# First completely understand the algorithm and the re-implement
+POS = {}
+POS['Papa']		=	['NP']
+POS['ate']		=	['Verb']
+POS['the']		=	['Det']
+POS['a']		=	['Det']
+POS['caviar']	= 	['Noun']
+POS['spoon']	=	['Noun']
+POS['with']		=	['P']
+
+sent = ['Papa','ate','the','caviar','with','a','spoon']
+chart = [[] for i in range(0,len(sent)+1)]
+earley_parser()
+
+def earley_parser():
+	initial_state = ['root','.','S',[0,0]]
+	enqueue(initial_state,0)
+	for i in range(0,len(sent)+1):
+		print i
+		# To-do for all states of the
+		current_index_in_chart = 0
+		while current_index_in_chart != len(chart[i]):
+			# print len(chart),i, len(chart[i]), current_index_in_chart
+			state = chart[i][current_index_in_chart]
+			# print state
+			if incomplete(state):
+				# find the thing next to '.'
+				if state[state.index('.')+1] in terminals:
+					scanner(state)
+				else :
+					predictor(state)
+			else :
+				completer(state)
+			current_index_in_chart += 1
+
+def enqueue(state,chart_index):
+	found = False
+	for current_state in chart[chart_index]:
+		# if chart_index == 1:
+		# 	print state, current_state
+		if current_state == state:
+			found = True
+	if found == False:
+		chart[chart_index].append(state)
+		# print state
 
 def incomplete(state):
-	print "_______________incomplete_________________________"
-	if state[len(state)-2] == '.':
-		return False
-	else :
+	if state[len(state)-2] != '.':
 		return True
+	else :
+		return False
 
-def predictor(state):		# A -> alpha.B Beta, [i,j]		# find the location of B
-	print "________________predictor________________________"
-	index_B = state.index('.') + 1
-	print state, len(state), index_B, state[index_B]
+def predictor(state):
+	index_B = state.index('.')+1
 	j = state[len(state)-1][1]
 	B = state[index_B]
-	if B in grammar.keys():
-		for sub_list in grammar[B]:
-			if len(sub_list) == 3:
-				enqueue([B,'.',sub_list[0],sub_list[1],sub_list[2],[j,j]],j)
-			if len(sub_list) == 2:
-				enqueue([B,'.',sub_list[0],sub_list[1],[j,j]],j)
-			else:
-				enqueue([B,'.',sub_list[0],[j,j]],j)
-			print sub_list, sub_list[0]
-# something wrong here
-def scanner(state,j):			# A -> alpha.B Beta, [i,j]
-	print "_______________scanner________________________________"
-	# find the location of B
-	index_B = state.index('.') + 1
+	if B in grammar:
+		for rhs_rule in grammar[B]: #rhs_rule is the RHS of the grammar rule
+			new_state = [B,'.']
+			for element in rhs_rule:
+				new_state.append(element)
+			new_state.append([j,j])
+			enqueue(new_state, j)
+
+def scanner(state):
+	index_B = state.index('.')-1
 	B = state[index_B]
-	print B, POS[sent[j]]
-	if B == sent[j]:
-		enqueue([POS[B][0],sent[j],'.',[j,j+1]],j)
+	j = state[len(state)-1][1]
+	if j < len(sent):
+		if B in POS[sent[j]]:
+			enqueue([B,sent[j],'.',[j,j+1]],j+1)
 
 def completer(state):
-	print "_________________completer_____________________________"
-	k = state[len(state)-1][1]
-	for sub_list in chart[state[len(state)-1][0]]:
-		if sub_list[sub_list.index('.')-1] == state[0]:
-			new_state = state
-			index_B = sub_list.index('.') + 1
-			new_state[index_B], new_state[index_B-1] = new_state[index_B-1],new_state[index_B]
-			enqueue(new_state,k)
+	j = state[len(state)-1][0]		#Note this 0
+	for current_state in chart[j]:
+		if current_state[current_state.index('.')+1] == state[0]:
+			new_state = copy.deepcopy(current_state)
+			new_state[current_state.index('.')]   = current_state[current_state.index('.')+1]
+			new_state[current_state.index('.')+1] =	current_state[current_state.index('.')] 
+			new_state[len(new_state)-1][1] = state[len(state)-1][1]
+			enqueue(new_state,state[len(state)-1][1])
 
-EarleyParser(grammar)
+chart = [[] for i in range(0,len(sent)+1)]
+earley_parser()
 
 print chart
 
-for list_ in chart[0]:
-	print list_
+count = 0
+for list_ in chart:
+	print count
+	for sub_list in list_:
+		print sub_list
+	count += 1
